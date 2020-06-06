9529890b52ef78412468176f27f1abc0aece073b
Kamil Nowocin <kamil.nowocin@intive.com>
2020-06-06 15:16:42 +0200

    feat: travis tests

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-mkdir target/github-pages
-git clone --single-branch --branch gh-pages https://kamil-nowocin:$GITHUB:TOKEN@github.com/kamil-nowocin/Test_Automation-automationpractice target/github-pages
+#mkdir target
+## shellcheck disable=SC2164
+#cd target
+#mkdir github-pages
+git clone --single-branch --branch gh-pages https://kamil-nowocin:$GITHUBTOKEN@github.com/kamil-nowocin/Test_Automation-automationpractice target/github-pages
 sleep 5
 cp -R target/github-pages/history build/allure-results
+# shellcheck disable=SC2164
 cd build
 allure generate --clean
 sleep 10
 cd ..
 cp -R build/allure-report/* target/github-pages/
+# shellcheck disable=SC2164
 cd target/github-pages/
 git config user.name "Travis CI"
 git config user.email "deploy@travis-ci.org"
